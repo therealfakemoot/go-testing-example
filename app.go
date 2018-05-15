e10601c8a811d370a0684b5e33ffdc7137f8462b
Nick Dumas <drakonik@gmail.com>
2018-05-15 23:11:03 +0000

    Flesh out the FileManipulator interface.
    I'm on the fence about the interface to declare for the f value in
    FileManipulator. I want this to be a type that can support both a
    bytes.Buffer and an os.File. bytes.Buffer does not implement Seek(). I
    may replace this with io.ReadWriter and just use whole-buffer
    copy/replace instead of seeking.

--- app.go
+++ app.go
@@ -1,8 +1,27 @@
 package main
 
-import "os"
+import (
+	"io"
+)
 
-// Clean removes a given file/directory. If there is an error, it is of type *PathError
-func Clean(path string) error {
-	return os.Remove(path)
+// Appender represents a value that can write bytes to the end of a buffer.
+type Appender interface {
+	Append([]byte) (int, error)
+}
+
+// Prepender represents a value that can write bytes to the start of a buffer.
+type Prepender interface {
+	Prepend([]byte) (int, error)
+}
+
+// FileManipulator implements Appender and Prepender
+type FileManipulator struct {
+	f io.ReadWriteSeeker
+}
+
+// Prepend adds argument to the beginning of the file.
+func (fm *FileManipulator) Prepend(s string) {}
+
+// Append adds argument to the end of the file.
+func (fm *FileManipulator) Append(s string) {
 }
