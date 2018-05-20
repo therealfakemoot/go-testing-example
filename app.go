b1634e56ed16e78f78dbe050bbff9f04de03fdf1
Nick Dumas <drakonik@gmail.com>
2018-05-20 20:18:37 +0000

    Remove useless interfaces. Change FileManipulator back to using os.File.
    Append test now passes.

--- app.go
+++ app.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"os"
 )
-
-// Appender represents a value that can write bytes to the end of a buffer.
-type Appender interface {
-	Append([]byte) (int, error)
-}
-
-// Prepender represents a value that can write bytes to the start of a buffer.
-type Prepender interface {
-	Prepend([]byte) (int, error)
-}
 
 // FileManipulator implements Appender and Prepender
 type FileManipulator struct {
-	f io.ReadWriteSeeker
+	f *os.File
 }
 
 // Prepend adds argument to the beginning of the file.
-func (fm *FileManipulator) Prepend(s string) {}
+func (fm *FileManipulator) Prepend(s string) {
+	b := bytes.NewBuffer(nil)
+	fm.f.Seek(0, 0)
+	b.WriteString(s)
+	io.Copy(b, fm.f)
+	fm.f.Truncate(0)
+	fm.f.Write(b.Bytes())
+	fm.f.Seek(0, 0)
+}
 
 // Append adds argument to the end of the file.
 func (fm *FileManipulator) Append(s string) {
+	b := bytes.NewBuffer(nil)
+	fm.f.Seek(0, 0)
+	io.Copy(b, fm.f)
+	b.WriteString(s)
+	fm.f.Truncate(0)
+	fm.f.Write(b.Bytes())
+	fm.f.Seek(0, 0)
 }
