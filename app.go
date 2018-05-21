63b738c3b7995ab2077c56718bbc50cbc3a33153
Nick Dumas <drakonik@gmail.com>
2018-05-21 23:17:49 +0000

    Passing tests, details below.
    The test failures were caused by using Truncate() without a Seek() call.
    Per the official docs, Truncate() changes a file's contents without
    changing the location of the 'cursor' within that file. The previous
    code was truncating and then writing at an offset, causing null bytes
    and test failures.
    
    By using Truncate, WriteAt, and Seek in that order I'm able to copy the
    file contents over and leave the file in a state fit for manipulation by
    the rest of the application.

--- app.go
+++ app.go
@@ -13,22 +13,22 @@ type FileManipulator struct {
 
 // Prepend adds argument to the beginning of the file.
 func (fm *FileManipulator) Prepend(s string) {
-	b := bytes.NewBuffer(nil)
+	var b bytes.Buffer
 	fm.f.Seek(0, 0)
 	b.WriteString(s)
-	io.Copy(b, fm.f)
+	io.Copy(&b, fm.f)
 	fm.f.Truncate(0)
-	fm.f.Write(b.Bytes())
+	fm.f.WriteAt(b.Bytes(), 0)
 	fm.f.Seek(0, 0)
 }
 
 // Append adds argument to the end of the file.
 func (fm *FileManipulator) Append(s string) {
-	b := bytes.NewBuffer(nil)
+	var b bytes.Buffer
 	fm.f.Seek(0, 0)
-	io.Copy(b, fm.f)
+	io.Copy(&b, fm.f)
 	b.WriteString(s)
 	fm.f.Truncate(0)
-	fm.f.Write(b.Bytes())
+	fm.f.WriteAt(b.Bytes(), 0)
 	fm.f.Seek(0, 0)
 }
